d3f7658e83cc6753d3f7607ccd842918489a3010
gael.sola <gaelsola@hotmail.fr>
2021-01-23 16:13:47 +0700

    finished 3.2

--- R/basic.R
+++ R/basic.R
@@ -1,5 +1,7 @@
 
 library(shiny)
+library(tidyverse)
+library(lubridate)
 
 animals <- c("dog", "cat", "mouse", "bird", "other", "I hate animals")
 
@@ -14,6 +16,9 @@ ui <- fluidPage(
   numericInput("num", "Number one", value = 0, min = 0, max = 100),
   sliderInput("num2", "Number two", value = 50, min = 0, max = 100),
   sliderInput("rng", "Range", value = c(10, 20), min = 0, max = 100),
+  sliderInput("ex", "When should we deliv?", value = ymd("2020-09-17"), min = ymd("2020-09-16"), max = ymd("2020-09-23"), timeFormat = "%F"),
+  sliderInput("ex2", "Select a value:", value = 0, min = 0, max = 100, step = 5, animate = TRUE),
+  numericInput("number", "Select a value", value = 150, min = 0, max = 1000, step = 50), ## Step defines the interval between each value when using top/bottom arrows
   
   ## dates
   dateInput("dob", "When were you born?"),
@@ -36,6 +41,12 @@ ui <- fluidPage(
     multiple = TRUE
   ),
   checkboxGroupInput("animal2", "What animals do you like?", animals),
+  selectInput(
+    "state3", "Choose a state:",
+    list(`East Coast` = list("NY", "NJ", "CT"),
+         `West Coast` = list("WA", "OR", "CA"),
+         `Midwest` = list("MN", "WI", "IA"))
+  ),
   
   ## file
   fileInput("upload", NULL),
